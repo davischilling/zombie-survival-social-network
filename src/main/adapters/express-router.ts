b390d68e2ff8cdcf9821141082dea1eb018e513b
davi schilling <davischilling.dev@gmail.com>
2022-10-01 18:45:15 -0300

    feat: ensure ExpressRouter Adapter responds with 200 and correct data

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,9 +4,10 @@ import { RequestHandler } from 'express'
 type Adapter = (controller: Controller) => RequestHandler
 
 export const adaptExpressRoute: Adapter = (controller) => async (req, res) => {
-  await controller.handle({
+  const { statusCode, data } = await controller.handle({
     ...req.body,
     ...req.query,
     ...req.params,
   })
+  res.status(statusCode).json(data)
 }
