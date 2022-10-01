6b223bb36aff6aea63e12988b677fb843294174b
davi schilling <davischilling.dev@gmail.com>
2022-10-01 13:48:06 -0300

    feat: should define a new id for the entity if it was not passed to the constructor

--- src/data/entities/user.ts
+++ src/data/entities/user.ts
@@ -2,14 +2,19 @@ import { ItemTypes, LocationType, SexTypes, UserModel } from '@/domain/models'
 
 class User implements UserModel {
   id?: string
-  name!: string
-  age!: number
-  sex!: SexTypes
-  location!: LocationType
-  itens!: ItemTypes[]
-  isInfected!: boolean
+  name: string
+  age: number
+  sex: SexTypes
+  location: LocationType
+  itens: ItemTypes[]
+  isInfected: boolean
 
-  constructor({ name, age, sex, location, isInfected, itens }: UserModel) {
+  constructor({ id, name, age, sex, location, isInfected, itens }: UserModel) {
+    if (!id) {
+      this.id = 'new_id'
+    } else {
+      this.id = id
+    }
     this.name = name
     this.age = age
     this.sex = sex
