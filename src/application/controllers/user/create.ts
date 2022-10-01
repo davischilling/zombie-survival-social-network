6910002712e47524f13135ae792fdac1f487b6fb
davi schilling <davischilling.dev@gmail.com>
2022-10-01 18:06:46 -0300

    feat: ensure CreateUserController calls createUserService.handle with correct params

---
+++ src/application/controllers/user/create.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '@/application/controllers/Controller'
+import { ICreateUserService } from '@/domain/use-cases/user'
+
+export class CreateUserController extends Controller {
+  constructor(private readonly createUserService: ICreateUserService) {
+    super()
+  }
+
+  async perform(httpRequest: any): Promise<HttpResponse<any>> {
+    await this.createUserService.handle(httpRequest)
+    return {
+      statusCode: 200,
+      data: {},
+    }
+  }
+}
