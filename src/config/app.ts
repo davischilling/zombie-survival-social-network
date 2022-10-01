0505ab768deddd0327768e9076c9fd4a6522574d
davi schilling <davischilling.dev@gmail.com>
2022-10-01 12:18:38 -0300

    feat: swagger module implemented to document the api endpoints

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import swaggerIi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+app.use('/api-docs', swaggerIi.serve, swaggerIi.setup(swaggerFile))
 
 export { app }
