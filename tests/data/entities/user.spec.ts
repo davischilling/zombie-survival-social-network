364e84dbe18a7a408e9bc0ec15cc5f63183bd4b4
davi schilling <davischilling.dev@gmail.com>
2022-10-01 14:04:48 -0300

    test: should call idGenerator if id was not passed returning a valid id

--- tests/data/entities/user.spec.ts
+++ tests/data/entities/user.spec.ts
@@ -1,11 +1,13 @@
-import User from '@/data/entities/user'
+import User, { IIdGenerator } from '@/data/entities/user'
 import { SexTypes, UserModel } from '@/domain/models'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('Share', () => {
+  let idGeneratorMock: MockProxy<IIdGenerator>
   let userModelMock: UserModel
   let sut: User
 
-  beforeEach(() => {
+  beforeAll(() => {
     userModelMock = {
       id: 'id',
       name: '',
@@ -18,10 +20,12 @@ describe('Share', () => {
       isInfected: false,
       itens: [],
     }
+    idGeneratorMock = mock()
+    idGeneratorMock.perform.mockReturnValue('new_id')
   })
 
   it('should fill the attrs on the constructor with correct params', () => {
-    sut = new User(userModelMock)
+    sut = new User(userModelMock, idGeneratorMock)
 
     expect(sut).toEqual(userModelMock)
   })
@@ -29,18 +33,30 @@ describe('Share', () => {
   it('should define a new id for the entity if it was not passed to the constructor', () => {
     const { id, ...userWithNoId } = userModelMock
 
-    sut = new User(userWithNoId)
+    sut = new User(userWithNoId, idGeneratorMock)
 
     expect(sut.id).toBeDefined()
   })
 
   it('should update an user correctly', () => {
-    sut = new User(userModelMock)
+    sut = new User(userModelMock, idGeneratorMock)
     sut.id = 'any_user_id'
 
     sut.name = 'updated_name'
-    const updatedUser = new User({ ...sut, ...{ name: 'updated_name' } })
+    const updatedUser = new User(
+      { ...sut, ...{ name: 'updated_name' } },
+      idGeneratorMock
+    )
 
     expect(updatedUser).toEqual(sut)
+  })
+
+  it('should call idGenerator if id was not passed returning a valid id', () => {
+    const { id, ...userWithNoId } = userModelMock
+
+    sut = new User(userWithNoId, idGeneratorMock)
+
+    expect(idGeneratorMock.perform).toHaveBeenCalledTimes(1)
+    expect(sut.id).toBe('new_id')
   })
 })
