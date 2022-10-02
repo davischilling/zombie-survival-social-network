e7b8294461ac5cfaa5c31f40d59fea1f2938ccde
davi schilling <davischilling.dev@gmail.com>
2022-10-02 18:37:00 -0300

    refactor: change user repository test behavior removing location as a unique schema

--- src/infra/db/repositories/User.ts
+++ src/infra/db/repositories/User.ts
@@ -1,26 +1,19 @@
 import { IRepository } from '@/data/contracts'
 import User from '@/data/entities/user'
-import LocationSchema from '@/infra/db/schemas/Location'
-import UserSchema from '@/infra/db/schemas/User'
+import { UserSchema } from '@/infra/db/schemas/User'
 
 export class UserRepository implements IRepository {
   async create(params: any): Promise<string | null> {
-    const { location, ...userAttrs } = params
-    const newUser: any = await UserSchema.create(userAttrs, {
-      fields: ['id', 'name', 'age', 'sex', 'items', 'isInfected'],
+    const { location, id: _id, ...userAttrs } = params
+    const newUser: any = await UserSchema.create({
+      _id,
+      ...userAttrs,
+      ...location,
     })
-    const { latitude, longitude } = location
-    await LocationSchema.create(
-      { latitude, longitude },
-      {
-        fields: ['latitude', 'longitude'],
-      }
-    )
     return newUser.id
   }
   async find(params: any): Promise<{ items: number; data: User[] }> {
     const users: any = await UserSchema.findAll({
-      attributes: ['id', 'name', 'age', 'sex', 'items', 'isInfected'],
       where: { ...params },
     })
     return {
--- src/infra/db/schemas/User.ts
+++ src/infra/db/schemas/User.ts
@@ -1,15 +1,19 @@
 import { ItemEnumTypes, SexEnumTypes } from '@/domain/models'
-import { sequelize } from '@/main/config/database'
+import { db } from '@/main/config/database'
 import { DataTypes } from 'sequelize'
 
-const UserSchema = sequelize.define(
-  'User',
+export const UserSchema = db.define(
+  'user',
   {
     id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    _id: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
@@ -22,20 +26,37 @@ const UserSchema = sequelize.define(
     sex: {
       type: DataTypes.ENUM,
       allowNull: false,
-      values: [SexEnumTypes.male, SexEnumTypes.male],
+      values: [SexEnumTypes.male, SexEnumTypes.female],
     },
     items: {
-      type: DataTypes.ARRAY(DataTypes.ENUM<ItemEnumTypes>()),
+      type: DataTypes.ARRAY(
+        DataTypes.ENUM({
+          values: [
+            ItemEnumTypes.water,
+            ItemEnumTypes.medicine,
+            ItemEnumTypes.food,
+            ItemEnumTypes.ammunition,
+          ],
+        })
+      ),
       allowNull: false,
     },
     isInfected: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
     },
+    latitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     timestamps: true,
+    freezeTableName: true,
+    tableName: 'user',
   }
 )
-
-export default UserSchema
