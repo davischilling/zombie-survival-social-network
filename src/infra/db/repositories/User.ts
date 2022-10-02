a3b2602c367af374838e376c44fb00d47c827d90
davi schilling <davischilling.dev@gmail.com>
2022-10-01 23:37:47 -0300

    feat: ensure UserRepository creates a new User and Location and return an user id

---
+++ src/infra/db/repositories/User.ts
@@ -0,0 +1,42 @@
+import { IRepository } from '@/data/contracts'
+import User from '@/data/entities/user'
+import LocationSchema from '@/infra/db/schemas/Location'
+import UserSchema from '@/infra/db/schemas/User'
+
+export class UserRepository implements IRepository {
+  async create(params: any): Promise<string | null> {
+    const { location, ...userAttrs } = params
+    const newUser: any = await UserSchema.create(userAttrs, {
+      fields: ['id', 'name', 'age', 'sex', 'items', 'isInfected'],
+    })
+    const { latitude, longitude } = location
+    await LocationSchema.create(
+      { latitude, longitude },
+      {
+        fields: ['latitude', 'longitude'],
+      }
+    )
+    return newUser.id
+  }
+  async find(params: any): Promise<{ items: number; data: User[] }> {
+    return {
+      items: 1,
+      data: [],
+    }
+  }
+  async findById(id: string): Promise<User> {
+    return {} as User
+  }
+  async findOneByParam(param: any): Promise<User> {
+    return {} as User
+  }
+  async findByIdAndUpdate(
+    id: string,
+    updatedObj: User
+  ): Promise<string | null> {
+    return ''
+  }
+  async findByIdAndDelete(id: string): Promise<string | null> {
+    return ''
+  }
+}
