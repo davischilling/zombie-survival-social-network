62779fcd03ac9368871874a9f1cfc4793745fab7
davi schilling <davischilling.dev@gmail.com>
2022-10-03 20:13:47 -0300

    feat: implemented add item to user route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -2,6 +2,7 @@ import { adaptExpressRoute as adaptCtrl } from '@/main/adapters/express-router'
 import {
   makeCreateUserController,
   makeUpdateUserLocationController,
+  makeAddItemToUserController,
 } from '@/main/factories/controllers/user'
 import { Router } from 'express'
 
@@ -11,6 +12,11 @@ const entityNameRoutes = async (router: Router) => {
   router.patch(
     '/users/location/:id',
     adaptCtrl(await makeUpdateUserLocationController())
+  )
+
+  router.post(
+    '/users/:userId/items',
+    adaptCtrl(await makeAddItemToUserController())
   )
 }
 
