1a080a5c1e14321b1e761ca167ee7b161b911441
davi schilling <davischilling.dev@gmail.com>
2022-10-03 19:43:00 -0300

    feat: ensure AddItemtoUserController calls addItemToUserService.handle with correct params

---
+++ src/application/controllers/user/add-item.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '@/application/controllers/Controller'
+import { IAddItemToUserService } from '@/domain/use-cases/user'
+
+export class AddItemToUserController extends Controller {
+  constructor(private readonly addItemToUserService: IAddItemToUserService) {
+    super()
+  }
+
+  async perform(httpRequest: any): Promise<HttpResponse<any>> {
+    await this.addItemToUserService.handle(httpRequest)
+    return {
+      statusCode: 200,
+      data: {},
+    }
+  }
+}
