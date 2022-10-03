6d1ac371972659083ea477541c3c614a1b68e52e
davi schilling <davischilling.dev@gmail.com>
2022-10-02 21:45:12 -0300

    feat: enrure UpdateUserLocationController calls updateUserLocationService.handle with correct params

---
+++ src/application/controllers/user/update-location.ts
@@ -0,0 +1,22 @@
+import { Controller, HttpResponse } from '@/application/controllers/Controller'
+import { IUpdateUserLocationService } from '@/domain/use-cases/user'
+
+export class UpdateUserLocationController extends Controller {
+  constructor(
+    private readonly updateUserLocationService: IUpdateUserLocationService
+  ) {
+    super()
+  }
+
+  async perform(httpRequest: any): Promise<HttpResponse<any>> {
+    await this.updateUserLocationService.handle(httpRequest)
+    // return {
+    //   statusCode: 201,
+    //   data: { message: 'User created' },
+    // }
+    return {
+      statusCode: 200,
+      data: {},
+    }
+  }
+}
