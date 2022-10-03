3e1939839b874f3fffe4f10a1fb4f691a39b9337
davi schilling <davischilling.dev@gmail.com>
2022-10-03 20:06:22 -0300

    test: itemRepository should create a new Item for the user returning its id

---
+++ tests/infra/db/repositories/item.spec.ts
@@ -0,0 +1,41 @@
+import { ItemEnumTypes, ItemModel } from '@/domain/models'
+import { ItemRepository } from '@/infra/db/repositories'
+import { ItemSchema } from '@/infra/db/schemas/Item'
+import { faker } from '@faker-js/faker'
+
+jest.mock('@/infra/db/schemas/Item')
+
+describe('Item Repository', () => {
+  let itemModelMock: ItemModel
+  let fakeItemSchema: jest.Mocked<typeof ItemSchema>
+  let sut: ItemRepository
+
+  beforeAll(() => {
+    itemModelMock = {
+      id: faker.datatype.uuid(),
+      name: ItemEnumTypes.water,
+      points: 4,
+      userId: faker.datatype.uuid(),
+    }
+    fakeItemSchema = ItemSchema as jest.Mocked<typeof ItemSchema>
+    fakeItemSchema.create.mockResolvedValue(itemModelMock)
+  })
+
+  beforeEach(() => {
+    sut = new ItemRepository()
+  })
+
+  it('should create a new Item for the user returning its id', async () => {
+    const id = await sut.create(itemModelMock)
+
+    const { id: _id, ...itemAttrs } = itemModelMock
+
+    expect(fakeItemSchema.create).toHaveBeenCalledWith({
+      _id,
+      ...itemAttrs,
+    })
+    expect(fakeItemSchema.create).toHaveBeenCalledTimes(1)
+
+    expect(id).toBe(itemModelMock.id)
+  })
+})
