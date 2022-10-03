574c18d1235658e4665ee97a609a750422d2bf3c
davi schilling <davischilling.dev@gmail.com>
2022-10-02 23:31:18 -0300

    feat: implemented update user location route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,17 @@
 import { adaptExpressRoute as adaptCtrl } from '@/main/adapters/express-router'
-import { makeCreateUserController } from '@/main/factories/controllers/user'
+import {
+  makeCreateUserController,
+  makeUpdateUserLocationController,
+} from '@/main/factories/controllers/user'
 import { Router } from 'express'
 
 const entityNameRoutes = async (router: Router) => {
   router.post('/users', adaptCtrl(await makeCreateUserController()))
+
+  router.patch(
+    '/users/location/:id',
+    adaptCtrl(await makeUpdateUserLocationController())
+  )
 }
 
 export default entityNameRoutes
