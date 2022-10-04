41855706351b1727f1ac26ad6233d67180867464
davi schilling <davischilling.dev@gmail.com>
2022-10-04 17:36:52 -0300

    test: survivalReportService should calculate percentages correctly
    SurvivalReportService should calculate percentageOfInfectedUsers and percentageOfNonInfectedUsers
    correctly

--- tests/data/services/report/survival-report.spec.ts
+++ tests/data/services/report/survival-report.spec.ts
@@ -2,16 +2,35 @@ import { IRepository } from '@/data/contracts'
 import { SurvivalReportService } from '@/data/services/report'
 import { ItemModel, UserModel } from '@/domain/models'
 import { ISurvivalReportService } from '@/domain/use-cases/report'
+import { faker } from '@faker-js/faker'
 import { mock, MockProxy } from 'jest-mock-extended'
+
+import { generateUser } from './mocks/generateUser'
 
 describe('Survival Report Service', () => {
   let userRepo: MockProxy<IRepository<UserModel>>
   let itemRepo: MockProxy<IRepository<ItemModel>>
+  let firstUser: UserModel
+  let secondUser: UserModel
+  let thirdUser: UserModel
+  let fourthUser: UserModel
+  let fifthUser: UserModel
+
   let sut: ISurvivalReportService
 
   beforeAll(() => {
+    firstUser = generateUser(faker.datatype.uuid(), false)
+    secondUser = generateUser(faker.datatype.uuid(), false)
+    thirdUser = generateUser(faker.datatype.uuid(), true)
+    fourthUser = generateUser(faker.datatype.uuid(), true)
+    fifthUser = generateUser(faker.datatype.uuid(), true)
+
     userRepo = mock()
     itemRepo = mock()
+    userRepo.find.mockResolvedValue({
+      items: 5,
+      data: [firstUser, secondUser, thirdUser, fourthUser, fifthUser],
+    })
   })
 
   beforeEach(() => {
@@ -23,5 +42,13 @@ describe('Survival Report Service', () => {
 
     expect(userRepo.find).toHaveBeenCalledWith({})
     expect(userRepo.find).toHaveBeenCalledTimes(1)
+  })
+
+  it('should calculate percentageOfInfectedUsers and percentageOfNonInfectedUsers correctly', async () => {
+    const { percentageOfInfectedUsers, percentageOfNonInfectedUsers } =
+      await sut.handle()
+
+    expect(percentageOfNonInfectedUsers).toBe(40)
+    expect(percentageOfInfectedUsers).toBe(60)
   })
 })
