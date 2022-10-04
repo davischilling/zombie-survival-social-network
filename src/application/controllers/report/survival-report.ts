94d7a4ecc18f4ff2b22abca56098d67109495e77
davi schilling <davischilling.dev@gmail.com>
2022-10-04 19:32:04 -0300

    feat: ensure SurvivalReportController calls survivalReportService.handle with correct params

---
+++ src/application/controllers/report/survival-report.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '@/application/controllers/Controller'
+import { ISurvivalReportService } from '@/domain/use-cases/report'
+
+export class SurvivalReportController extends Controller {
+  constructor(private readonly survivalReportService: ISurvivalReportService) {
+    super()
+  }
+
+  async perform(): Promise<HttpResponse<any>> {
+    await this.survivalReportService.handle()
+    return {
+      statusCode: 200,
+      data: {},
+    }
+  }
+}
