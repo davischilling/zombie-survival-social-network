2c1db0befb1e6308be545e4de199977f1eb7b650
davi schilling <davischilling.dev@gmail.com>
2022-10-04 12:03:23 -0300

    feat: change user repository findById and findByOneParam return behavior

--- src/infra/db/repositories/User.ts
+++ src/infra/db/repositories/User.ts
@@ -1,8 +1,35 @@
 import { IRepository } from '@/data/contracts'
 import User from '@/data/entities/user'
+import { SexTypes } from '@/domain/models'
 import { UserSchema } from '@/infra/db/schemas/User'
 
+export type SqliteUserModel = {
+  id: number
+  _id: string
+  name: string
+  age: number
+  sex: SexTypes
+  latitude: string
+  longitude: string
+  isInfected: boolean
+  createdAt: Date
+  updatedAt: Date
+}
+
 export class UserRepository implements IRepository {
+  static sqliteToDTO(params: SqliteUserModel) {
+    const { id, createdAt, updatedAt, _id, latitude, longitude, ...userAttrs } =
+      params
+    return {
+      id: _id,
+      location: {
+        latitude,
+        longitude,
+      },
+      ...userAttrs,
+    }
+  }
+
   async create(params: any): Promise<string> {
     const { location, id: _id, ...userAttrs } = params
     const newUser: any = await UserSchema.create({
@@ -22,16 +49,24 @@ export class UserRepository implements IRepository {
     }
   }
   async findById(id: string): Promise<User | null> {
-    const user = await UserSchema.findOne({
+    const user: any = await UserSchema.findOne({
       where: { _id: id },
     })
-    return user as any
+    if (user) {
+      const { dataValues } = user
+      return UserRepository.sqliteToDTO(dataValues)
+    }
+    return user
   }
   async findOneByParam(params: any): Promise<User | null> {
-    const user = await UserSchema.findOne({
+    const user: any = await UserSchema.findOne({
       where: { ...params },
     })
-    return user as any
+    if (user) {
+      const { dataValues } = user
+      return UserRepository.sqliteToDTO(dataValues)
+    }
+    return user
   }
   async findByIdAndUpdate(id: string, updatedObj: User): Promise<string> {
     const { id: _id, location, ...userAttrs } = updatedObj
