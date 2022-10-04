b58a3d884b3a99997df0c146771033c72abf01a3
davi schilling <davischilling.dev@gmail.com>
2022-10-04 12:45:13 -0300

    test: itemsExchangeService should call UserRepo.findById for each user passing correct params

---
+++ tests/data/services/user/item-exchange.spec.ts
@@ -0,0 +1,83 @@
+import { IRepository } from '@/data/contracts'
+import { ItemsExchangeService } from '@/data/services/user'
+import { ItemEnumTypes, UserModel } from '@/domain/models'
+import {
+  IItemsExchangeService,
+  ItemsExchangeDTOType,
+} from '@/domain/use-cases/user'
+import { faker } from '@faker-js/faker'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+import { generateUser } from './mocks/generateUser'
+
+jest.mock('@/data/entities/user')
+
+describe('Items Exchange Service', () => {
+  let dealerId: string
+  let clientId: string
+  let dealerFound: UserModel
+  let clientFound: UserModel
+
+  let userRepo: MockProxy<IRepository>
+  let itemsExchangeDTO: ItemsExchangeDTOType
+  let sut: IItemsExchangeService
+
+  beforeAll(() => {
+    clientId = faker.datatype.uuid()
+    dealerId = faker.datatype.uuid()
+
+    itemsExchangeDTO = {
+      dealerId,
+      dealerItems: [
+        {
+          id: faker.datatype.uuid(),
+          name: faker.helpers.arrayElement([
+            ItemEnumTypes.water,
+            ItemEnumTypes.medicine,
+            ItemEnumTypes.food,
+            ItemEnumTypes.ammunition,
+          ]),
+          points: faker.datatype.number({
+            min: 1,
+            max: 4,
+          }),
+          userId: dealerId,
+        },
+      ],
+      clientId,
+      clientItems: [
+        {
+          id: faker.datatype.uuid(),
+          name: faker.helpers.arrayElement([
+            ItemEnumTypes.water,
+            ItemEnumTypes.medicine,
+            ItemEnumTypes.food,
+            ItemEnumTypes.ammunition,
+          ]),
+          points: faker.datatype.number({
+            min: 1,
+            max: 4,
+          }),
+          userId: clientId,
+        },
+      ],
+    }
+    userRepo = mock()
+    dealerFound = generateUser(dealerId, false)
+    clientFound = generateUser(clientId, false)
+    userRepo.findById.mockResolvedValue(dealerFound)
+    userRepo.findById.mockResolvedValue(clientFound)
+  })
+
+  beforeEach(() => {
+    sut = new ItemsExchangeService(userRepo)
+  })
+
+  it('should call UserRepo.findById for each user passing correct params', async () => {
+    await sut.handle(itemsExchangeDTO)
+
+    expect(userRepo.findById).toHaveBeenCalledWith(dealerId)
+    expect(userRepo.findById).toHaveBeenCalledWith(clientId)
+    expect(userRepo.findById).toHaveBeenCalledTimes(2)
+  })
+})
