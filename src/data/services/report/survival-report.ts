11bd3332d741c562b09a3689d78b7ab52d9c5d74
davi schilling <davischilling.dev@gmail.com>
2022-10-04 17:53:00 -0300

    feat: ensure SurvivalReportService calls itemRepo.find with correct params

--- src/data/services/report/survival-report.ts
+++ src/data/services/report/survival-report.ts
@@ -16,6 +16,11 @@ export class SurvivalReportService implements ISurvivalReportService {
     const allUsers = await this.userRepo.find({})
     const { percentageOfNonInfectedUsers, percentageOfInfectedUsers } =
       calculatePercentage(allUsers.data)
+    const itemsByUserPromises: any[] = []
+    allUsers.data.forEach((user) => {
+      itemsByUserPromises.push(this.itemRepo.find({ userId: user.id }))
+    })
+    const itemsByUser = await Promise.all(itemsByUserPromises)
     return {
       percentageOfNonInfectedUsers,
       percentageOfInfectedUsers,
