eb1d01451687c6854aeee1b669337ac6e0ec5188
davi schilling <davischilling.dev@gmail.com>
2022-10-04 12:04:55 -0300

    feat: implemented mark user as infected route

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -2,6 +2,7 @@ import { adaptExpressRoute as adaptCtrl } from '@/main/adapters/express-router'
 import {
   makeCreateUserController,
   makeUpdateUserLocationController,
+  makeMarkUserAsInfectedController,
 } from '@/main/factories/controllers/user'
 import { Router } from 'express'
 
@@ -9,8 +10,13 @@ const entityNameRoutes = async (router: Router) => {
   router.post('/users', adaptCtrl(await makeCreateUserController()))
 
   router.patch(
-    '/users/location/:id',
+    '/users/:id/location',
     adaptCtrl(await makeUpdateUserLocationController())
+  )
+
+  router.patch(
+    '/users/:id/infected',
+    adaptCtrl(await makeMarkUserAsInfectedController())
   )
 }
 
