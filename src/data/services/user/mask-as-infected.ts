92ccb8c2f5d5d1c4ec661da2d3b0d9820aa06735
davi schilling <davischilling.dev@gmail.com>
2022-10-04 02:14:34 -0300

    feat: ensure MarkUserAsinfectedService throws if invalid or not found user

--- src/data/services/user/mask-as-infected.ts
+++ src/data/services/user/mask-as-infected.ts
@@ -1,11 +1,12 @@
 import { IRepository } from '@/data/contracts'
+import { UserModel } from '@/domain/models'
 import {
   IMarkUserAsInfectedService,
   MarkUserAsInfectedUseCase,
 } from '@/domain/use-cases/user'
 
 export class MarkUserAsInfectedService implements IMarkUserAsInfectedService {
-  constructor(private readonly userRepo: IRepository) {}
+  constructor(private readonly userRepo: IRepository<UserModel>) {}
 
   async handle({
     id,
@@ -17,7 +18,20 @@ export class MarkUserAsInfectedService implements IMarkUserAsInfectedService {
     const snitchOneFound = await this.userRepo.findById(snitchOneId)
     const snitchTwoFound = await this.userRepo.findById(snitchTwoId)
     const snitchThreeFound = await this.userRepo.findById(snitchThreeId)
-    if (userToUpdate.isInfected) {
+    if (
+      !userToUpdate ||
+      !snitchOneFound ||
+      !snitchTwoFound ||
+      !snitchThreeFound
+    ) {
+      throw new Error('not_found')
+    }
+    if (
+      userToUpdate.isInfected ||
+      snitchOneFound.isInfected ||
+      snitchTwoFound.isInfected ||
+      snitchThreeFound.isInfected
+    ) {
       throw new Error('invalid_user')
     }
   }
