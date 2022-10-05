be02ed5d98ac64b60c1f8134f71684862c52cce4
davi schilling <davischilling.dev@gmail.com>
2022-10-05 14:23:16 -0300

    test: createItemRoute should return 201 and a success message

--- tests/main/routes/item/add-item.spec.ts
+++ tests/main/routes/item/add-item.spec.ts
@@ -1,5 +1,6 @@
 import { UserModel } from '@/domain/models'
 import { UserRepository } from '@/infra/db/repositories'
+import { ItemSchema } from '@/infra/db/schemas/Item'
 import { UserSchema } from '@/infra/db/schemas/User'
 import { app } from '@/main/config/app'
 import { db } from '@/main/config/database'
@@ -25,12 +26,6 @@ const createUser = async (idParam: string, isInfectedParam: boolean) => {
 }
 
 describe('Create Item Route - POST /items/:userId', () => {
-  let user: UserModel
-
-  beforeAll(async () => {
-    user = generateUser(faker.datatype.uuid(), faker.datatype.boolean())
-  })
-
   beforeEach(async () => {
     await db.sync()
   })
@@ -43,5 +38,25 @@ describe('Create Item Route - POST /items/:userId', () => {
 
     expect(statusCode).toBe(404)
     expect(body).toEqual({ error: 'USER not_found' })
+  })
+
+  it('should return 201 and a success message', async () => {
+    const newUser = await createUser(faker.datatype.uuid(), false)
+
+    const beforeItemCreation: any = await ItemSchema.findAll()
+
+    expect(beforeItemCreation.length).toBe(0)
+
+    const { statusCode, body } = await request(app)
+      .post(`/items/${newUser.id}`)
+      .set('Accept', 'application/json')
+      .send({ name: 'water' })
+
+    expect(statusCode).toBe(201)
+    expect(body.message).toBe('Item added to user with success')
+
+    const afterItemCreation: any = await ItemSchema.findAll()
+
+    expect(afterItemCreation.length).toBe(1)
   })
 })
